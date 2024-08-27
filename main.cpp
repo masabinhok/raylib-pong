34d92f9305d4b9e30b0d682c6dae1513a8d4c2c4
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-08-27 10:12:25 +0545

    create a blank scren & game loop

--- main.cpp
+++ main.cpp
@@ -4,5 +4,25 @@
 using namespace std;
 
 int main () {
-return 0;
+    cout<<"Starting the game"<<endl;
+    //defining the screen resolution
+    const int screen_width = 1280;
+    const int screen_height = 800;
+
+    //initializing the game window
+    InitWindow(screen_width, screen_height, "Pong Game");
+
+    //setting the fps
+    SetTargetFPS(60);
+
+    //Game loop
+    while(WindowShouldClose() == false)
+    {
+        BeginDrawing();   //draw the canvas
+ 
+        EndDrawing();   //close the canvas
+    }
+
+    CloseWindow(); //close the window
+    return 0;
 }
