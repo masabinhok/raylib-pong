b33aab0bd367f587b01b4fb52caf36e2a253e38c
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-08-27 10:48:22 +0545

    move the ball around

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+class Ball {
+    public :
+    float x, y;
+    int speed_x, speed_y, radius;
+
+    void Draw () {
+        DrawCircle(x, y , radius, WHITE);
+    }
+
+    void Update() {
+        x += speed_x;
+        y += speed_y;
+    }
+};
+
+Ball ball;
+
+
 int main () {
     cout<<"Starting the game"<<endl;
     //defining the screen resolution
@@ -15,15 +33,27 @@ int main () {
     //setting the fps
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.speed_x=7;
+    ball.speed_y=7;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+
     //Game loop
     while(WindowShouldClose() == false)
     {
         BeginDrawing();   //draw the canvas
+
+            //updating 
+            ball.Update();
+
             //drawing
-            DrawCircle(screen_width/2, screen_height/2 , 20, WHITE);
+            ClearBackground(BLACK);
+            DrawLine(screen_width/2 , 0 , screen_width/2 , screen_height, WHITE);
+            ball.Draw();
             DrawRectangle(10, (screen_height/2 - 60), 25, 120, BLUE);
             DrawRectangle((screen_width- 35), (screen_height/2 - 60), 25, 120, RED);
-            DrawLine(screen_width/2 , 0 , screen_width/2 , screen_height, WHITE);
+
         EndDrawing();   //close the canvas
     }
 
